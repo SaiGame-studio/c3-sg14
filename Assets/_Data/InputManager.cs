73b262048252bdeb66428e4ad8c59bfe9acf1587
SaiHome <sai@saisaga.com>
2022-10-18 22:40:50 +0700

    e5

--- Assets/_Data/InputManager.cs
+++ Assets/_Data/InputManager.cs
@@ -10,7 +10,6 @@ public class InputManager : MonoBehaviour
     [SerializeField] protected Vector3 mouseWorldPos;
     public Vector3 MouseWorldPos { get => mouseWorldPos; }
 
-
     private void Awake()
     {
         if (InputManager.instance != null) Debug.LogError("Only 1 InputManager allow to exist");
