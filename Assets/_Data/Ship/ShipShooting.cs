fb8ede2d191a2e913e8616432b37d945efc311f6
SaiHome <sai@saisaga.com>
2022-10-26 00:04:56 +0700

    e9

--- Assets/_Data/Ship/ShipShooting.cs
+++ Assets/_Data/Ship/ShipShooting.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class ShipShooting : MonoBehaviour
 {
     [SerializeField] protected bool isShooting = false;
+    [SerializeField] protected float shootDelay = 1f;
+    [SerializeField] protected float shootTimer = 0f;
     [SerializeField] protected Transform bulletPrefab;
+
+    void Update()
+    {
+        this.IsShooting();
+    }
 
     private void FixedUpdate()
     {
         this.Shooting();
     }
 
-
     protected virtual void Shooting()
     {
         if (!this.isShooting) return;
+
+        this.shootTimer += Time.fixedDeltaTime;
+        if (this.shootTimer < this.shootDelay) return;
+        this.shootTimer = 0;
 
         Vector3 spawnPos = transform.position;
         Quaternion rotation = transform.parent.rotation;
         Instantiate(this.bulletPrefab, spawnPos, rotation);
         Debug.Log("Shooting");
+    }
+
+    protected virtual bool IsShooting()
+    {
+        this.isShooting = InputManager.Instance.OnFiring == 1;
+        return this.isShooting;
     }
 }
