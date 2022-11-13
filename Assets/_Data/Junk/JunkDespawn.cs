cc2c49a88f905818d0ebc8a8c216a1a9c5673860
SaiHome <sai@saisaga.com>
2022-11-14 02:49:58 +0700

    e19

---
+++ Assets/_Data/Junk/JunkDespawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkDespawn : DespawnByDistance
+{
+    protected override void DespawnObject()
+    {
+        JunkSpawner.Instance.Despawn(transform.parent);
+    }
+
+    protected override void ResetValue()
+    {
+        base.ResetValue();
+        this.disLimit = 25f;
+    }
+}
