c18c8b68994c2235ec0815e9649ba479144f8b08
SaiHome <sai@saisaga.com>
2022-11-20 23:32:04 +0700

    e-20

--- Assets/_Data/Junk/JunkFly.cs
+++ Assets/_Data/Junk/JunkFly.cs
@@ -1,12 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JunkFly : ParentFly
 {
+    [SerializeField] protected float minCamPos = -16f;
+    [SerializeField] protected float maxCamPos = 16f;
+
     protected override void ResetValue()
     {
         base.ResetValue();
         this.moveSpeed = 0.5f;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        this.GetFlyDirection();
+    }
+
+    protected virtual void GetFlyDirection()
+    {
+        Vector3 camPos = GameCtrl.Instance.MainCamera.transform.position;
+        Vector3 objPos = transform.parent.position;
+
+        camPos.x += Random.Range(this.minCamPos, this.maxCamPos);
+        camPos.z += Random.Range(this.minCamPos, this.maxCamPos);
+
+        Vector3 diff = camPos - objPos;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.parent.rotation = Quaternion.Euler(0f, 0f, rot_z);
+
+        //this.direction = (pos - transform.position).normalized;
+        Debug.DrawLine(objPos, objPos + diff * 7, Color.red, Mathf.Infinity);
     }
 }
