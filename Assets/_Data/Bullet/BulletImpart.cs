1985608c0cb89bfee405283562238e65935c759e
SaiHome <sai@saisaga.com>
2022-12-07 20:39:19 +0700

    E29

--- Assets/_Data/Bullet/BulletImpart.cs
+++ Assets/_Data/Bullet/BulletImpart.cs
@@ -37,5 +37,21 @@ public class BulletImpart : BulletAbstract
     protected virtual void OnTriggerEnter(Collider other)
     {
         this.bulletCtrl.DamageSender.Send(other.transform);
+        this.CreateImpactFX(other);
+    }
+
+    protected virtual void CreateImpactFX(Collider other)
+    {
+        string fxName = this.GetImpactFX();
+
+        Vector3 hitPos = transform.position;
+        Quaternion hitRot = transform.rotation;
+        Transform fxImpact = FXSpawner.Instance.Spawn(fxName, hitPos, hitRot);
+        fxImpact.gameObject.SetActive(true);
+    }
+
+    protected virtual string GetImpactFX()
+    {
+        return FXSpawner.impact1;
     }
 }
