5c340f72152bd7af6ebbfcc657d9c85af460ddce
SaiHome <sai@saisaga.com>
2023-03-05 21:57:19 +0700

    e72

--- Assets/_Data/InputManager.cs
+++ Assets/_Data/InputManager.cs
@@ -13,6 +13,9 @@ public class InputManager : MonoBehaviour
     [SerializeField] protected float onFiring;
     public float OnFiring { get => onFiring; }
 
+    protected Vector4 direction;
+    public Vector4 Direction => direction;
+
     void Awake()
     {
         if (InputManager.instance != null) Debug.LogError("Only 1 InputManager allow to exist");
@@ -22,6 +25,7 @@ public class InputManager : MonoBehaviour
     void Update()
     {
         this.GetMouseDown();
+        this.GetDirectionByKeyDown();
     }
 
     void FixedUpdate()
@@ -37,5 +41,25 @@ public class InputManager : MonoBehaviour
     protected virtual void GetMousePos()
     {
         this.mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    protected virtual void GetDirectionByKeyDown()
+    {
+        this.direction.x = Input.GetKeyDown(KeyCode.A) ? 1 : 0;
+        if(this.direction.x == 0) this.direction.x = Input.GetKeyDown(KeyCode.LeftArrow) ? 1 : 0;
+
+        this.direction.y = Input.GetKeyDown(KeyCode.D) ? 1 : 0;
+        if (this.direction.y == 0) this.direction.y = Input.GetKeyDown(KeyCode.RightArrow) ? 1 : 0;
+        
+        this.direction.z = Input.GetKeyDown(KeyCode.W) ? 1 : 0;
+        if (this.direction.z == 0) this.direction.z = Input.GetKeyDown(KeyCode.UpArrow) ? 1 : 0;
+        
+        this.direction.w = Input.GetKeyDown(KeyCode.S) ? 1 : 0;
+        if (this.direction.w == 0) this.direction.w = Input.GetKeyDown(KeyCode.DownArrow) ? 1 : 0;
+
+        //if (this.direction.x == 1) Debug.Log("Left");
+        //if (this.direction.y == 1) Debug.Log("Right");
+        //if (this.direction.z == 1) Debug.Log("Up");
+        //if (this.direction.w == 1) Debug.Log("Down");
     }
 }
