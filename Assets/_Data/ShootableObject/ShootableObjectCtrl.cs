721c392b9e1a6433d6c5003256cdee91e06a6fdc
SaiHome <sai@saisaga.com>
2023-03-12 20:49:20 +0700

    e76

--- Assets/_Data/ShootableObject/ShootableObjectCtrl.cs
+++ Assets/_Data/ShootableObject/ShootableObjectCtrl.cs
@@ -26,6 +26,9 @@ public abstract class ShootableObjectCtrl : SaiMonoBehaviour
     [SerializeField] protected Spawner spawner;
     public Spawner Spawner => spawner;
 
+    [SerializeField] protected DamageReceiver damageReceiver;
+    public DamageReceiver DamageReceiver => damageReceiver;
+
     protected override void LoadComponents()
     {
         base.LoadComponents();
@@ -36,6 +39,14 @@ public abstract class ShootableObjectCtrl : SaiMonoBehaviour
         this.LoadObjMovement();
         this.LoadObjLookAtTarget();
         this.LoadSpawner();
+        this.LoadDamageReceiver();
+    }
+
+    protected virtual void LoadDamageReceiver()
+    {
+        if (this.damageReceiver != null) return;
+        this.damageReceiver = transform.GetComponentInChildren<DamageReceiver>();
+        Debug.LogWarning(transform.name + ": LoadDamageReceiver", gameObject);
     }
 
     protected virtual void LoadSpawner()
