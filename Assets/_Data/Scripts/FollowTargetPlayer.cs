5df89c2d190364c361ea735b0ee7a6cc4b22a373
Sai Demo <saidemo@saisaga.com>
2023-07-31 03:35:28 +0700

    e115

---
+++ Assets/_Data/Scripts/FollowTargetPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTargetPlayer: FollowTarget
+{
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadTarget();
+    }
+
+    protected virtual void LoadTarget()
+    {
+        if (this.target != null) return;
+        this.target = GameObject.Find("PlayerShipsManager").transform;
+        Debug.LogWarning(transform.name + ": LoadTarget", gameObject);
+    }
+}
