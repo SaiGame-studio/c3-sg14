30a0c998edcebd6504184a9952aa78d9fc66f91f
Sai Demo <saidemo@saisaga.com>
2023-08-21 00:26:42 +0700

    change damage

--- Assets/_Data/Ship/ShipAutoShoot.cs
+++ Assets/_Data/Ship/ShipAutoShoot.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class ShipAutoShoot : ObjShooting
 {
+    [SerializeField] protected double damage = 1;
+
     protected override bool IsShooting()
     {
         this.isShooting = true;
         return this.isShooting;
+    }
+
+    public virtual void SetDamage(double damage)
+    {
+        this.damage = damage;
     }
 }
