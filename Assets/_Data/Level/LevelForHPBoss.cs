49e0bb9cab78db11bace46a12286b59832979e7a
Sai Demo <saidemo@saisaga.com>
2023-09-01 13:34:54 +0700

    e133

---
+++ Assets/_Data/Level/LevelForHPBoss.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LevelForHPBoss : LevelDouble
+{
+    [Header("For Damage")]
+    public BossDamReceiver bossDamReceiver;
+    [SerializeField] protected int levelOffset = 7;
+
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        this.UpdateLevel();
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadDamageReceiver();
+    }
+
+    protected virtual void LoadDamageReceiver()
+    {
+        if (this.bossDamReceiver != null) return;
+        this.bossDamReceiver = transform.parent.GetComponent<BossDamReceiver>();
+        Debug.LogWarning(transform.name + ": LoadDamageReceiver", gameObject);
+    }
+
+    protected virtual void UpdateLevel()
+    {
+        int level = MapCtrl.Instance.MapLevel.LevelCurrent + this.levelOffset;
+        this.SetLevel(level);
+        this.bossDamReceiver.SetHPMax(this.number);
+    }
+}
